38260ca383e3913b8fcd9072ed66563d4cb89a20
Mal <128443854+Iqmalmal@users.noreply.github.com>
2023-09-14 17:18:00 +0800

    Revert "Revert "Frontend merge""

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -4,7 +4,8 @@
   @endif
 
   @include('partials._search')
-
+  @include('partials._category')
+  
   <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
     @unless(count($listings) == 0)
