073a431caf617ce8f764b018fe964017cb655a2b
Iqmalmal <miqmaldaniel.zainal@student.gmi.edu.my>
2023-10-08 18:44:55 +0800

    show listing
    UI update

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+
   <head>
     <!-- Include Owl Carousel CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
@@ -6,7 +7,6 @@
   </head>
 
   <style>
-    /* Add this CSS to your stylesheet */
     .owl-carousel .owl-stage {
         margin: 0; /* Remove any margin */
         padding: 0; /* Remove any padding */
@@ -14,166 +14,234 @@
 
     .owl-carousel .owl-stage-outer {
         position: relative;
-        top: 0%;
+        width: 800px;
         margin-bottom: 80%;
-        z-index: 0;
+        z-index: -1;
     }
 
-    .owl-container {
-    position: relative;
-    top: 40px;
-    left: 0;
-    z-index: 1; /* Ensure it's above other elements */
+    .product-details{
+        width: 100%;
+        padding: 60px ;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .image-slider{
+        position: relative;
+        background-image: url('../img/product\ image\ 1.png');
+        background-size: cover;
+    }
+
+    .product-images{
+        position: absolute;
+        bottom: 20px;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 90%;
+        background: #fff;
+        border-radius: 5px;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        height: 100px;
+        grid-gap: 10px;
+        padding: 10px;
+    }
+
+    .product-images img.active{
+        opacity: 0.5;
+    }
+
+    .details{
+        width: 50%;
+    }
+
+    .details .product-brand{
+        text-transform: capitalize;
+        font-size: 30px;
+    }
+
+    .details .product-short-des{
+        font-size: 25px;
+        line-height: 30px;
+        height: auto;
+        margin: 15px 0 30px;
+    }
+
+    .product-price{
+        font-weight: 700;
+        font-size: 30px;
+    }
+
+    .product-actual-price{
+        font-size: 30px;
+        opacity: 0.5;
+        text-decoration: line-through;
+        margin: 0 20px;
+        font-weight: 300;
+    }
+
+    .product-discount{
+        color: #ff7d7d;
+        font-size: 20px;
+    }
+
+    .product-sub-heading{
+        font-size: 30px;
+        text-transform: uppercase;
+        margin: 60px 0 10px;
+        font-weight: 300;
+    }
+
+    .size-radio-btn{
+        display: inline-block;
+        width: 80px;
+        height: 80px;
+        text-align: center;
+        font-size: 20px;
+        border: 1px solid #383838;
+        border-radius: 50%;
+        margin: 10px;
+        margin-left: 0;
+        line-height: 80px;
+        text-transform: uppercase;
+        color: #383838;
+        cursor: pointer;
+    }
+
+    .size-radio-btn.check{
+        background: #383838;
+        color: #fff;
+    }
+
+    .detail-des{
+        margin-top: -400px;
+        padding: 0 10vw;
+        text-transform: capitalize;
+        font-size: 20px;
+    }
+
+    .heading{
+        font-size: 30px;
+        margin-bottom: 30px;
+    }
+
+    .des{
+        color: #383838;
+        line-height: 25px;
     }
   </style>
 
-  <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back</a>
-  <div class="flex mx-4 items-center justify-center">
-    <div class="w-1/2 p-4">
-      <div class="owl-container">
-        <div class="owl-carousel owl-theme">
-          @foreach (json_decode($listing->images) as $image)
-          <div class="item">
-            <img style="max-height: 50vh; max-width: 50vh;" class="w-auto ml-60" src="{{ asset('storage/' . $image) }}" alt="Product Image">
-          </div>
-          @endforeach
+  <section class="product-details">
+    <div class="owl-container">
+      <div class="owl-carousel owl-theme">
+        @foreach (json_decode($listing->images) as $image)
+        <div class="item">
+          <img style="max-height: 50vh; max-width: 50vh;" class="w-auto ml-60" src="{{ asset('storage/' . $image) }}" alt="Product Image">
         </div>
+        @endforeach
       </div>
     </div>
-    <div class="w-1/2 p-4 mr-60">
-      <x-card class="p-10">
-        <div class="inline-block text-black mb-4">
-          <h3 class="text-2xl mb-2">
-            <p><b>RM{{$listing->price}}</b> : {{$listing->product_name}}</p>
-          </h3>
-          <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
 
-          <x-listing-tags :tagsCsv="$listing->tags" />
+      <div class="details">
+        <h2 class="product-brand"><b>{{$listing->product_name}}</b></h2>
+        <p class="product-short-des">{{$listing->seller}}</p>
+        <span class="product-price">RM{{$listing->price}}</span>
+        {{-- <span class="product-actual-price">$200</span>
+        <span class="product-discount">( 50% off )</span> --}}
+        <br>
+        <form action="/addCart">
+        @csrf
 
-          <div class="text-lg my-4">
-            <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
-          </div>
-
-          <form action="/addCart">
-            @csrf
-          <div class="mb-5">
-            <h5 class="text-2xl font-bold mb-5">Product Variants</h5>
-            <div class=" inline-block text-lg space-y-6"></div>
-              @if ($productVariantData->isNotEmpty())
-              <span>Colour:</span>
-              <div>
-                <select name="variant-select" id="variant-colour">
-                  @foreach ($productVariantData as $variant)
-                    <option value="-">-</option>
-                    @foreach([$productVariantData->first()->colour_1, $productVariantData->first()->colour_2, $productVariantData->first()->colour_3] as $color)
-                    <option value="{{$color}}">{{$color}}</option>
-                    @endforeach
-                  @endforeach
-                </select>
-              </div>
-              <br>
-              <span>Size:</span>
-              <div>
-                <select name="variant-select" id="variant-size">
-                  @foreach ($productVariantData as $variant)
-                    <option value="-">-</option>
-                    @foreach([$productVariantData->first()->size_1, $productVariantData->first()->size_2, $productVariantData->first()->size_3] as $size)
-                    <option value="{{$size}}">{{$size}}</option>
-                    @endforeach
-                  @endforeach
-                </select>
-              </div>
-              <br>
-              <span>Capacity:</span>
-              <div>
-                
-                <select name="variant-select" id="variant-capacity">
-                  @foreach ($productVariantData as $variant)
-                    <option value="-">-</option>
-                    @if (!empty($productVariantData->first()->capacity_1))
-                      @foreach([$productVariantData->first()->capacity_1, $productVariantData->first()->capacity_2, $productVariantData->first()->capacity_3] as $capacity)
-                      <option value="{{$capacity}}">{{$capacity}}</option>
-                      @endforeach
-                    @endif
-                  @endforeach
-                </select>
-                
-              </div>
-
-              @else
-                <span> empty </span>
-              @endif
-            </div>
-          </div>
-
-          <div class="border border-gray-200 w-full mb-5"></div>
-          <div class="flex gap-3 mb-7">
-
+        <div class="mb-5">
+          <h5 class="text-2xl mb-5">Product Variants</h5>
+          <div class=" inline-block text-lg space-y-6"></div>
+            @if ($productVariantData->isNotEmpty())
             
-
-              <input type="hidden" name="listing-id" value="{{ $listing->id }}">
-              <input type="hidden" name="listing-name" value="{{ $listing->product_name }}">
-              <input type="hidden" name="listing-price" value="{{ $listing->price }}">
-              <input type="hidden" name="selected-variants[]" id="selected-variants" value="" multiple>
-
-              <button type="submit" class="bg-green-600 text-white rounded py-2 px-4 hover:bg-black">Add to cart</button>
-            </form>
-
-            @if (Auth::check())
-              <a href="mailto:{{$listing->email}}?subject=Chaft%3A%20{{$listing->product_name}}&body=Hi%2C%20is%20this%20still%20available%3F">
-                <button type="submit" class="bg-blue-600 text-white rounded py-2 px-4 hover:bg-black"> Contact Seller </button>
-              </a>
-            @else
-              <a href="/register">
-                <button type="submit" class="bg-blue-600 text-white rounded py-2 px-4 hover:bg-black"> Contact Seller </button>
-              </a>
-            @endif
-            {{-- <a href="mailto:{{$listing->email}}?subject=Chaft%3A%20{{$listing->product_name}}&body=Hi%2C%20is%20this%20still%20available%3F">
-              <button type="submit" class="bg-blue-600 text-white rounded py-2 px-4 hover:bg-black"> Contact Seller </button>
-            </a> --}}
-          </div>
-          <div class="border border-gray-200 w-full mb-6"></div>
-          <div>
-            <h3 class="text-3xl font-bold mb-4">Job Description</h3>
-            <div class="text-lg space-y-6">
-            {!! nl2br(e($listing->description)) !!}
-
+            <span>Colour:</span>
+            <div>
+              <select name="variant-select" id="variant-colour">
+                @foreach ($productVariantData as $variant)
+                  <option value="-">-</option>
+                  @foreach([$productVariantData->first()->colour_1, $productVariantData->first()->colour_2, $productVariantData->first()->colour_3] as $color)
+                  <option value="{{$color}}">{{$color}}</option>
+                  @endforeach
+                @endforeach
+              </select>
             </div>
+            
+            <br>
+            
+            <span>Size:</span>
+            <div>
+              <select name="variant-select" id="variant-size">
+                @foreach ($productVariantData as $variant)
+                  <option value="-">-</option>
+                  @foreach([$productVariantData->first()->size_1, $productVariantData->first()->size_2, $productVariantData->first()->size_3] as $size)
+                  <option value="{{$size}}">{{$size}}</option>
+                  @endforeach
+                @endforeach
+              </select>
+            </div>
+            
+            <br>
+            
+            <span>Capacity:</span>
+            <div>
+              
+              <select name="variant-select" id="variant-capacity">
+                @foreach ($productVariantData as $variant)
+                  <option value="-">-</option>
+                  @if (!empty($productVariantData->first()->capacity_1))
+                    @foreach([$productVariantData->first()->capacity_1, $productVariantData->first()->capacity_2, $productVariantData->first()->capacity_3] as $capacity)
+                    <option value="{{$capacity}}">{{$capacity}}</option>
+                    @endforeach
+                  @endif
+                @endforeach
+              </select>
+            
+            </div>
+
+            @else
+              <span> empty </span>
+            @endif
           </div>
         </div>
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-        <script>
-          $(document).ready(function () {
-            $(".owl-carousel").owlCarousel({
-              items: 1, // Number of items to display
-              loop: true, // Enable looping
-              autoplay: true, // Enable autoplay
-              autoplayTimeout: 3000, // Autoplay interval in milliseconds
-              autoplayHoverPause: true, // Pause on hover
-              dots: false, // Disable dots
-            });
-          });
-        </script>
+        <div class="border border-gray-200 w-full mb-5"></div>
+        <div class="flex gap-3 mb-7">
 
-        <script>
+          
 
-          const selectedVariantsInput = document.getElementById('selected-variants');
+            <input type="hidden" name="listing-id" value="{{ $listing->id }}">
+            <input type="hidden" name="listing-name" value="{{ $listing->product_name }}">
+            <input type="hidden" name="listing-price" value="{{ $listing->price }}">
+            <input type="hidden" name="selected-variants[]" id="selected-variants" value="" multiple>
 
-          $(document).ready(function() {
-            $('#variant-colour, #variant-size, #variant-capacity').change(function() {
-                var selectedVariants = [];
-                selectedVariants.push($('#variant-colour').val());
-                selectedVariants.push($('#variant-size').val());
-                selectedVariants.push($('#variant-capacity').val());
+            <button type="submit" class="bg-green-600 text-white rounded w-48 h-5 py-2 px-4 hover:bg-black">Add to cart</button>
+          </form>
 
-                console.log(selectedVariants); // This will log the array to the console
-                selectedVariantsInput.value = selectedVariants.join(', ');
-            });
-        });
-        </script>
-      </x-card>
-    </div>
-  </div>
+        <button class="btn">add to wishlist</button>
+      </div>
+  </section>
+
+  <section class="detail-des">
+    <h2 class="heading">description</h2>
+    <p class="des">{!! nl2br(e($listing->description)) !!}</p>
+  </section>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <script>
+    $(document).ready(function () {
+      $(".owl-carousel").owlCarousel({
+        items: 1, // Number of items to display
+        loop: true, // Enable looping
+        autoplay: false, // Enable autoplay
+        autoplayTimeout: 3000, // Autoplay interval in milliseconds
+        autoplayHoverPause: true, // Pause on hover
+        dots: false, // Disable dots
+        margin: 30, // Spacing between items (in px)
+      });
+    });
+  </script>
 </x-layout>
